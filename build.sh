6ea22d86d241bd4f2a481e153e8e1cd68b25cc6e
Ziga Stopinsek <sigi.kajzer@gmail.com>
2015-05-07 14:55:49 +0200

    exclude my tern-port files in xp

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 [ -f zemanta.xpi ] && rm zemanta.xpi;
 
-zip -r9 zemanta.xpi license.txt chrome.manifest install.rdf defaults/ chrome/ components/ loader/ -x \*.git/*
+zip -r9 zemanta.xpi license.txt chrome.manifest install.rdf defaults/ chrome/ components/ loader/ -x \*.git/* -x .tern-port
 printf "build finished.\n";
 
